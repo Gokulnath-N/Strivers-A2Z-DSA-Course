3499ad367546cec48f3bb07e63c5c3d3cdc99dd1
Gokulnath <gokutruns8@gmail.com>
2023-07-29 15:45:11 +0530

    Create SumOfEven&Odd.java

---
+++ LearnTheBasics/SumOfEven&Odd.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Main {
+	
+	public static void main(String[] args) {
+		// Write your code here
+	Scanner input = new Scanner(System.in);
+	int n = input.nextInt(),evenNumber=0,oddNumber=0;
+	while(n!=0)
+	{
+		int rem=n%10;
+		if(rem%2 == 0)
+		{
+			evenNumber += rem;
+		}
+		else
+		{
+			oddNumber += rem;
+		}
+		n=n/10;
+	}
+	System.out.print(evenNumber);
+	System.out.print(" " + oddNumber);
+		}
+}
