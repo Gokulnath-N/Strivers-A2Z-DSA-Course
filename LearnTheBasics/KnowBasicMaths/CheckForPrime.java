9c8191b5634fb87fc6e4c59eb06ead5619f89c51
gokulnath <gokulnath.n.tech@gmail.com>
2023-09-08 21:27:28 +0530

    new code for prime

--- LearnTheBasics/KnowBasicMaths/CheckForPrime.java
+++ LearnTheBasics/KnowBasicMaths/CheckForPrime.java
@@ -5,10 +5,18 @@ import java.util.Scanner;
 public class CheckForPrime {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int n= input.nextInt();
-        if(n%1==0 && n%n ==0)
-            System.out.println("prime number");
+        int n=input.nextInt();
+        int count=0;
+        for(int i=1;i<=n;i++)
+        {
+            if(n%i == 0)
+            {
+                count++;
+            }
+        }
+        if(count == 2)
+            System.out.println("Prime number");
         else
-            System.out.println("Not a prime number");
+            System.out.println("not a prime");
     }
 }
