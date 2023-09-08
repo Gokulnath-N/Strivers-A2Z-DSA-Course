8a36b87260eb0ae91e2bb475d72254fc817641f8
gokulnath <gokulnath.n.tech@gmail.com>
2023-09-08 21:34:30 +0530

    new code for prime

--- LearnTheBasics/KnowBasicMaths/CheckForPrime.java
+++ LearnTheBasics/KnowBasicMaths/CheckForPrime.java
@@ -18,5 +18,20 @@ public class CheckForPrime {
             System.out.println("Prime number");
         else
             System.out.println("not a prime");
+
+        /* optimal way
+        int cout=0;
+        for(int i=1;i*i<=n;i++)
+        {
+            if(n%i ==0)
+            count++;
+            if((n/i) != i)
+            count++;
+        }
+        if(count == 2)
+            System.out.println("Prime number");
+        else
+            System.out.println("not a prime");
+         */
     }
 }
