e16e57bf0feb57cb26aa2200c189628492e7e83d
gokulnath <gokulnath.n.tech@gmail.com>
2023-09-10 18:10:14 +0530

    fibonacci series using recursion completed

--- LearnTheBasics/BasicRecursion/ArrayReverse.java
+++ LearnTheBasics/BasicRecursion/ArrayReverse.java
@@ -4,16 +4,14 @@ package BasicRecursion;
 import java.util.Arrays;
 
 public class ArrayReverse {
+
     public static void main(String[] args) {
         int[] arr = {1,2,3,4,5};
         int n = arr.length - 1;
-        for(int i=n;i>=0;i--)
-        {
-            System.out.println(arr[i]);
-        }
         int[] revArr = reverse(arr,0,n);
         System.out.println(Arrays.toString(revArr));
     }
+
     static int[] reverse(int[] a,int i,int j)
     {
         if(i<j)
---
+++ LearnTheBasics/BasicRecursion/Fibonacci.java
@@ -0,0 +1,17 @@
+package BasicRecursion;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        int n=10;
+        for (int i=0;i<n;i++)
+        {
+            System.out.print(fibonacci(i)+" ");
+        }
+    }
+    static int fibonacci(int n)
+    {
+        if(n==0) return 0;
+        if(n==1 || n==2) return 1;
+        return fibonacci(n-1)+fibonacci(n-2);
+    }
+}
