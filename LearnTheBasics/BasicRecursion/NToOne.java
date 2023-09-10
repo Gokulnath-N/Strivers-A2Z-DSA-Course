c061d9c917b12595c3cf1fadd7f4bd3c05aec678
gokulnath <gokulnath.n.tech@gmail.com>
2023-09-10 15:35:34 +0530

    started with basic recursion problems

--- LearnTheBasics/BasicRecursion/NToOne.java
+++ LearnTheBasics/BasicRecursion/NToOne.java
@@ -4,11 +4,20 @@ public class NToOne {
     public static void main(String[] args) {
         int n=4;
         print(n,n);
+        print2(1,n);
     }
     static void print(int i, int n)
     {
         if(i<1) return;
         System.out.println(i);
         print(i-1,n);
+    }
+    // using backtracking
+    static  void print2(int i, int n)
+    {
+        if(i>n)
+            return;
+        print2(i+1,n);
+        System.out.println(i);
     }
 }
--- LearnTheBasics/BasicRecursion/OneToN.java
+++ LearnTheBasics/BasicRecursion/OneToN.java
@@ -6,12 +6,21 @@ public class OneToN {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int n = input.nextInt();
-        print(1,n);
+        print1(1,n);
+        print2(n,n);
     }
-    static void print(int i, int n)
+    static void print1(int i, int n)
     {
         if(i>n) return;
         System.out.println(i);
-        print(i+1,n);
+        print1(i+1,n);
+    }
+    // using bactracking
+    static void print2(int i, int n)
+    {
+        if(i<0)
+            return;
+        print2(i-1,n);
+        System.out.println(i);
     }
 }
