a449cfa7f2fd43aa44d246ee559aa998e95fc808
gokulnath <gokulnath.n.tech@gmail.com>
2023-11-18 13:01:31 +0530

    added HashMap approach to the CountArrElementsFreq

--- LearnTheBasics/BasicHashing/CountArrElementsFreq.java
+++ LearnTheBasics/BasicHashing/CountArrElementsFreq.java
@@ -7,7 +7,8 @@ public class CountArrElementsFreq {
         Scanner input = new Scanner(System.in);
         int[] arr = {10,5,10,15,10,5};
         int n = arr.length;
-        countFrequency(arr,n);
+        //countFrequency(arr,n);
+        frequencyUsingMap(arr,n);
     }
     static void countFrequency(int[] arr, int n)
     {
@@ -28,4 +29,24 @@ public class CountArrElementsFreq {
             System.out.println(arr[i]+ " " + count);
         }
     }
-}
+    static void frequencyUsingMap(int[] arr, int n)
+    {
+        HashMap<Integer,Integer> mp = new HashMap<>();
+        for(int i=0;i<n;i++)
+        {
+            int freq=0;
+            if(mp.containsKey(arr[i]))
+            {
+                mp.put(arr[i],mp.get(arr[i])+1);
+            }
+            else {
+                mp.put(arr[i],1);
+            }
+        }
+        for(Map.Entry<Integer,Integer> entry : mp.entrySet())
+        {
+            System.out.println(entry.getKey() + " " + entry.getValue());
+        }
+       }
+    }
+
